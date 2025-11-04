a21741233c59c615497121209f25038e44999cd9
mQhuy193 <mQhuy1903@gmail.com>
2025-11-04 22:51:10 +0700

    init code

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+const hostname = "localhost";
+const port = 8017;
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World Nodejs</h1>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(
+    `Hello Quang Huy, I'm running server at http://${hostname}:${port}`
+  );
+});
